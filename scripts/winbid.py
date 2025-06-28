52428db62a990b3e59cbde72d5dd287bdc045a1a
nightluo <1872194982@qq.com>
2025-06-28 23:40:45 +0800

    Update winbid.py

--- scripts/winbid.py
+++ scripts/winbid.py
@@ -12,6 +12,7 @@ from logging.handlers import RotatingFileHandler
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
+
 keyword_main = ["培训", "竞赛", "赋能", "会务", "营销"]
 keyword_list = ["交流活动", "辅助服务", "训战", "会议活动", "会议服务", "会展", "论坛", "实战", "服务支撑", "服务提质", "客户价值提升", "训练营"]
 keyword_list = keyword_main + keyword_list
