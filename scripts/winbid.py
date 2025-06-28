59f5a5b6a42f0b120ab37bb495a40995dd578868
nightluo <1872194982@qq.com>
2025-06-28 10:18:52 +0800

    Update winbid.py

--- scripts/winbid.py
+++ scripts/winbid.py
@@ -253,7 +253,7 @@ def lambda_handler(event, context):
     try:
         utc_now = datetime.now(timezone.utc)
         beijing_time = utc_now.astimezone(timezone(timedelta(hours=8)))        
-        end_time = beijing_time + timedelta(hours=8.1)
+        end_time = beijing_time + timedelta(hours=18)
         # start_time = beijing_time - timedelta(minutes=65)
         # logger.info(f"start_time: {start_time}")
         logger.info(f"end_time: {end_time}")
