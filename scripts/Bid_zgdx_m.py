635bcb4aaddcf31dcbe432f6497da04653c14e67
nightluo <1872194982@qq.com>
2025-07-07 10:14:24 +0800

    Update Bid_zgdx_m.py

--- scripts/Bid_zgdx_m.py
+++ scripts/Bid_zgdx_m.py
@@ -221,7 +221,7 @@ def lambda_handler(event, context):
                     continue
         except Exception as e:
             logger.error(f"中国电信，全局异常: {str(e)}")
-            error_send = webhook_test.send_text(f"全局异常: {str(e)}")
+            error_send = webhook_test.send_text(f"中国电信，全局异常: {str(e)}")
             
         if len(bid_total) >= 20:
             bid_total = bid_total[-6:]
