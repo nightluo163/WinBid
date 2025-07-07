3eac857fc0cc193dacf0f7dafb359bdcc693cbb9
nightluo <1872194982@qq.com>
2025-07-07 10:14:55 +0800

    Update Bid_zgtt_m.py

--- scripts/Bid_zgtt_m.py
+++ scripts/Bid_zgtt_m.py
@@ -226,7 +226,7 @@ def lambda_handler(event, context):
                     continue
         except Exception as e:
             logger.error(f"中国铁塔，全局异常: {str(e)}")
-            error_send = webhook_test.send_text(f"全局异常: {str(e)}")
+            error_send = webhook_test.send_text(f"中国铁塔，全局异常: {str(e)}")
             
         if len(bid_total) >= 20:
             bid_total = bid_total[-6:]
