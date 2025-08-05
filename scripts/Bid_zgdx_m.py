833c3177de1ed32661a1884963d13d0190fb4972
nightluo <1872194982@qq.com>
2025-08-05 14:51:55 +0800

    Update Bid_zgdx_m.py

--- scripts/Bid_zgdx_m.py
+++ scripts/Bid_zgdx_m.py
@@ -197,9 +197,9 @@ def lambda_handler(event, context):
     bid_total = []
     while beijing_time <= end_time:
         try:
-            # start_time = beijing_time - timedelta(days=1)
+            start_time = beijing_time - timedelta(days=2)
             # start_time = beijing_time - timedelta(hours=6)
-            start_time = beijing_time - timedelta(minutes=20)
+            # start_time = beijing_time - timedelta(minutes=20)
             logger.info(f"start_time: {start_time}")
             for keyword in keyword_list:
                 result = zgdx_search(keyword, start_time)
@@ -216,7 +216,7 @@ def lambda_handler(event, context):
                 
                 if message != '':
                     message = message[:-2]
-                    result = webhook.send_text(message)
+                    # result = webhook.send_text(message)
                     # result_test = webhook_test.send_text(message)
                     # logger.info(f"【调试】发送结果: {json.dumps(result)}")
                     # logger.info(f"【调试】发送结果: {json.dumps(result_test)}")
